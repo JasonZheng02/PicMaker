beeb220fabab600ae2c023be91569acd9232c775
Jason Zheng <jzheng02@stuy.edu>
2020-02-03 00:10:06 -0500

    Done

--- pic.py
+++ pic.py
@@ -1,11 +1,11 @@
 with open('pic.ppm', 'w') as f:
-    f.write('P3\n256 256\n255\n')
-    r = 64
+    f.write('P3\n512 512\n512\n')
+    r = 128
     g = 0
-    b = 210
+    b = 420
     i = 0
-    while i < 65536:
+    while i < 262144:
         f.write('{} {} {} '.format(r,g,b))
         g+=1
-        g%=256
+        g%=512
         i+=1
